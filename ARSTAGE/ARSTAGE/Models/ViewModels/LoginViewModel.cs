f1c1376d6e3c0cb9c9fbb95e495833d992a7fafa
Hà Đức Minh Triết <172189485+NahNoNaT@users.noreply.github.com>
2025-04-10 15:22:16 +0700

    login test
    test merch login

---
+++ ARSTAGE/ARSTAGE/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ARSTAGE.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Tên đăng nhập là bắt buộc")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
